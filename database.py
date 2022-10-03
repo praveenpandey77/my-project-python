2dc397583e0ad722df14521473c4f5943f52d0e5
praveenpandey77 <114177903+praveenpandey77@users.noreply.github.com>
2022-10-03 13:19:53 +0000

    image url corrected

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, text
+
+db_connection_maria = "mysql+pymysql://whv0mps52tdik0qyx5la:pscale_pw_3pd3RMotmgKLrFhfx40Y7UhUVvchWevXFINxk0S98CK@ap-south.connect.psdb.cloud/its-my-project?charset=utf8mb4"
+
+engine = create_engine(db_connection_maria,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    print(result.all())
+
+result_dict = []
+for row in result.all():
+    result_dict.append(dict(row))
+print(result_dict)
