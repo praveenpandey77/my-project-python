7c83da4259266ddaa02b31c11f154ab1c2e51594
praveenpandey77 <114177903+praveenpandey77@users.noreply.github.com>
2022-10-07 13:48:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+jobs = [{
+    'id': 1,
+    'title': 'data-analysis',
+    'location': 'pune',
+    'salary': '$4000'
+}, {
+    'id': 2,
+    'title': 'data-scientist',
+    'location': 'bangluru',
+    'salary': '$5000'
+}, {
+    'id': 3,
+    'title': 'front-end-engineer',
+    'location': 'hydrabad',
+    'salary': '$6000'
+}]
+
+
+@app.route("/")
+def hello_world():
+    return render_template('home.html', jobs=jobs)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(jobs)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
 
 @app.route("/")
 def hello_world():
